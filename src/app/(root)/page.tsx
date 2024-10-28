6261b70e53b4280ffea77da804ccefa8a309e077
ishwarthecodddr <ishwarthecoder@gmail.com>
2024-10-29 00:23:27 +0530

    hero

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="3xl">
-      Home
-    </div>
+    <section className="pink_container">
+      <h1 className="heading">
+        Pitch Your Startup , <br/> Connect with Entrepreneurs 
+      </h1>
+      <p className="sub-heading !max-w-3xl">
+        Submit Ideas, Vote On Pitches and Get noticed in virtual competitions.
+      </p>
+    </section>
   );
 }
